cd603367364d622c622fca68b9a36ca31d3f3f92
andres458 <andres_erazo30@hotmail.com>
2017-10-02 11:54:20 -0500

    Prueba

--- app/src/main/java/com/example/sistemas/calculonotas/acercaDe.java
+++ app/src/main/java/com/example/sistemas/calculonotas/acercaDe.java
@@ -9,7 +9,7 @@ public class acercaDe extends AppCompatActivity {
     private AdapterViewFlipper IVF;
 
     int images[]={R.drawable.imagen1,R.drawable.imagen2};
-    String names[]={"imagen1", "imagen2"};
+    String names[]={"imagen1","imagen2"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,13 +20,6 @@ public class acercaDe extends AppCompatActivity {
         IVF.setAdapter(custom);
         IVF.setFlipInterval(2000);
         IVF.setAutoStart(true);
-
-
-
-
-
-
-
 
     }
     public void salir(View v) {
